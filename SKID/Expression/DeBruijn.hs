0beb22675630e2a889d026dad8bdcf5787ef8a6d
NicklasBoto <nicklas.boto@gmail.com>
2020-03-01 20:36:04 +0100

    Added function Enc for encodings

--- SKID/Expression/DeBruijn.hs
+++ SKID/Expression/DeBruijn.hs
@@ -1,16 +1,20 @@
-module Expression.DeBruijn
-        ( Bλ (Idx, Abs, App, Unl)
+module DeBruijn
+        ( Bλ (Idx, Abs, App, Unl, Enc)
         ) where
 
 data Bλ = Idx Int
         | Abs Bλ
         | App Bλ Bλ
         | Unl String
-        deriving Show
+        | Enc
+        deriving Eq
 
-        {-
+-- App Enc (Abs (Idx 0)) == Abs (App Enc (Idx 0))
+-- Check β-equivalence (Remove abstractions, compute application) for Eq
+
 instance Show Bλ where
         show (Idx x)   = show x
         show (Abs s)   = "λ " ++ show s
-        show (App l r) = "(" ++ show l ++ show r ++ ")"
-        -}
+        show (App l r) = show l ++ " (" ++ show r ++ ")"
+        show (Unl s)   = "{" ++ s ++ "}"
+        show (Enc)     = "ζ"
