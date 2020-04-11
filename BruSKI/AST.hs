2b07d0724e5a6d3f6361734c7b162298e787ca55
NicklasBoto <nicklas.boto@gmail.com>
2020-04-11 17:33:25 +0200

    Changed Show instances, for windows stupid terminal

--- BruSKI/AST.hs
+++ BruSKI/AST.hs
@@ -1,15 +1,18 @@
 module AST
         ( Stmt (Assign, Express, Sequent)
         , Bλ (Idx, Abs, App, Unl, Enc, Prc, Fun)
+        , Iλ (Idx1, Abs1, App1, S1, K1, I1, Unl1, D1)
         ) where
 
 ---- Format Import
 import Data.List
 
+-- Stores BruSKI epxressions
 data Stmt = Assign String Bλ Integer
           | Express Bλ
           | Sequent [Stmt]
 
+-- Stores DebBruijn expressions
 data Bλ = Idx Integer
         | Abs Bλ
         | App Bλ Bλ
@@ -19,9 +22,22 @@ data Bλ = Idx Integer
         | Fun String [Bλ]
         deriving Eq
 
+-- Stores intermediate expressions
+data Iλ = Idx1 Int
+        | Abs1 Iλ
+        | App1 Iλ Iλ
+        | S1
+        | K1
+        | I1
+        | Unl1 String
+        | D1 String
+        deriving (Show, Eq)
+
 -- App Enc (Abs (Idx 0)) == Abs (App Enc (Idx 0))
 -- Check β-equivalence (Remove abstractions, compute application) for Eq
+-- Could just check SKI variant for equality
 
+{- Because windows terminal is mad at unicode
 instance Show Stmt where
         show (Assign s b a) = "[" ++ s ++ " , " ++ show b ++ ", " ++ show a ++ "]"
         show (Express  b) = "[!!, " ++ show b ++ "]"
@@ -35,6 +51,22 @@ instance Show Bλ where
         show (App l r)         = show l ++ " (" ++ show r ++ ")"
         show (Unl s)           = "{" ++ s ++ "}"
         show (Enc)             = "ζ"
+        show (Prc x)           = "%" ++ show x
+        show (Fun s a)         = show s ++ show a
+-}
+
+instance Show Stmt where
+        show (Assign s b a) = "[" ++ s ++ " , " ++ show b ++ ", " ++ show a ++ "]"
+        show (Express    b) = "[!!, " ++ show b ++ "]"
+        show (Sequent    s) = intercalate "\n" $ map show s
+
+instance Show Bλ where
+        show (Idx x)           = show x
+        show (Abs (App Enc r)) = "z(" ++ show (1 + decode r) ++ ")"
+        show (Abs s)           = "l " ++ show s
+        show (App l r)         = show l ++ " (" ++ show r ++ ")"
+        show (Unl s)           = "{" ++ s ++ "}"
+        show (Enc)             = "z"
         show (Prc x)           = "%" ++ show x
         show (Fun s a)         = show s ++ show a
 
