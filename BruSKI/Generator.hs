f59933a60701849651d41eafd461e7d9111acc70
NicklasBoto <nicklas.boto@gmail.com>
2020-04-13 02:04:37 +0200

    Foundation of symbol table generation done, BruSKI files can now be compiled (given that they use pure lambda and function application). Yay!

--- BruSKI/Generator.hs
+++ BruSKI/Generator.hs
@@ -1,28 +1,23 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
 module Generator
-        ( translate
-        , generate
-        , compile
+        (
         ) where
-
-{-
-I don't drink alcohol,
-I drink beer.
-    - Alaska State Troopers S4 E8
--}
 
 ---- Language Import
 import AST
-import Unlambda.AST
 
 ---- Parser Import
 import Parser -- unused
-import Encoding
 import Text.Regex
 
 ---- Translation Import
 import Translator
+
+---- Table Management Import
+import qualified Data.Map.Strict as Map
+import Control.Monad.State
+import Control.Monad
 
 ---- Debug Import
 import Unlambda.Run -- obviously unused
@@ -46,18 +41,52 @@ l ++ r = `ki
 
 -}
 
-type Symbol = (String, Bλ, [String])
+type Name = String
 
+type Unlambda = String
+
+-- type Symbol = (Name, (Bλ, [Unlambda]))
+type Symbol = (Name, Bλ)
+
+type SymbolTable = [Symbol]
+
+getFunction :: Name -> SymbolTable -> Bλ
+getFunction name table = case lookup name table of
+    Just s  -> s
+    Nothing -> error $ "Generator Error\nfunction " ++ name ++ " does not exist"
+
+prependTable :: Symbol -> State SymbolTable ()
+prependTable s = state $ \ss -> ((), (s:ss))
+
+partialApply :: Integer -> Unlambda -> [Unlambda]
+partialApply arity unl = take (fromInteger arity + 1) (iterate ('`':) unl)
+
+expandExpression :: Bλ -> SymbolTable -> Bλ
+expandExpression λ table = eE λ where
+    eE (Idx n)         = Idx n
+    eE (Abs λ)         = Abs (eE λ)
+    eE (App l r)       = App (eE l) (eE r)
+    eE (Fun name args) = eE $ foldl App (getFunction name table) args
+
+-- fix overapplication, see test for the bug
+
+generateTable :: [Stmt] -> State SymbolTable Unlambda
+generateTable [] = return []
+generateTable [Express λ] = generate . expandExpression λ <$> get
+generateTable ((Assign name λ a):ss) = do
+    curr <- get
+    let resolved = expandExpression λ curr
+    prependTable (name, resolved)
+    generateTable ss
+
+test = do
+    putStrLn . show $ runState (generateTable . parseString $ "a := λ0\nb := λλ1\n!! a{b, b, b}") []
 
 escapeUnl :: String -> String
 escapeUnl s = case matchRegexAll (mkRegex "%[0-9]+") s of
 --    Just (l, '%':i, r, _) -> l ++ (generate . translate . Idx1 . toInt) i  ++ escapeUnl r
-    Just (l, '%':i, r, _) -> l ++ " kuk " ++ escapeUnl r
+    Just (l, '%':i, r, _) -> l ++ " ? " ++ escapeUnl r
     Nothing               -> s
 
 -- YComb s i = "((λ (λ (1)((0)(0))) (λ (1)((0)(0)))) (λλλ ((2)(0)) ((1)(0)))) (λ0)"
 
-scrape :: [Symbol] -> Stmt -> Symbol
-scrape table (Assign name expr arity) = (name, sC expr, pApply (sC expr)) where
-    pApply s = take (fromInteger arity + 1) (iterate ('`':) (toUnl (s)))
-    sC = id
