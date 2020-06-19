dcd0a49a48e305ffb0aad98372747a7be9937e2c
NicklasBoto <nicklas.boto@gmail.com>
2020-06-19 16:12:52 +0200

    Added basic commands for compiler

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
-import Main.Utf8 (withUtf8)
 import Generator
+import Turtle
+import Paths_BruSKI (version)
+import Data.Version (showVersion)
+import qualified Sexy as Sexy
+
+mainSubroutine :: IO ()
+mainSubroutine = echo "Hello BruSKI!"
+
+-- compilerVersion :: IO ()
+-- compilerVersion = putStrLn (showVersion version)
+-- 
+-- verboseCompilerVersion :: IO ()
+-- verboseCompilerVersion = do
+--         compilerVersion
+--         -- echo "This compiler is currently in development!\nPlease notify me at [mail@nicklasbotö.se], or open an issue at [bruski.nicklasbotö.se] if you want a certain feature."
+--                                                                                                                                parseVersion :: Parser (IO ())
+-- parseVersion = (subcommand "version" "Show current compiler version" (pure verboseCompilerVersion))
+
+-- version
+version' :: IO()
+version' = putStrLn (showVersion version)
+
+verboseVersion :: IO()
+verboseVersion = do
+                 version'
+                 echo "This compiler is currently in development! Please notify me at [mail@nicklasbotö.se], or open an issue at [bruski.nicklasbotö.se] if you want a certain feature."
+
+parseVersion :: Parser (IO ())
+parseVersion =
+    (subcommand "version" "Show compiler version" (pure verboseVersion))
+
+parseMain :: Parser (IO ())
+parseMain = pure mainSubroutine
+
+parser :: Parser (IO ())
+parser = parseMain <|> parseVersion
+
+desc :: Description
+desc = "\n                                BruSKI\n                           DeBruijn -> SKI\n                       Version 0.4 - March 2020\n                           by Nicklas Botö"
 
 main :: IO ()
-main = putStrLn "Hello BruSKI!"
+main = do
+        cmd <- options desc parser
+        cmd
