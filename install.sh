ed94f950f4b223260dacc0e365a117efb4278e18
NicklasBoto <nicklas.boto@gmail.com>
2020-06-21 16:39:50 +0200

    Looks nicer

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Installing bruc..."
-echo "## stack build"
+echo "Bulding..."
 
 stack build
 
-echo "## copy to path"
+echo "Copying to path..."
 
 sudo cp ./.stack-work/dist/x86_64-linux-tinfo6/Cabal-3.0.1.0/build/bruc/bruc /usr/bin/
 
-echo "## cleaning up"
+echo "Cleaning up..."
 
 stack clean
 
-echo "## finished!"
+echo "Finished!"
