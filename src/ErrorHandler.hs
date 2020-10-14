4b4a7320e1b02a75acc09c8ad6101dc4f7956630
NicklasBoto <nicklas.boto@gmail.com>
2020-10-14 10:26:21 +0200

    Added better error presentation

--- src/ErrorHandler.hs
+++ src/ErrorHandler.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
 module ErrorHandler
-        (
+        ( failWith
         ) where
 
 data ParserError   = Missing | Unknown deriving Show
 data CompilerError = AbstractDangle | FreeVars deriving Show
+
+failWith e = errorWithoutStackTrace $ "*** " ++ e
 
 -- Hopefully handle, define, and show errors here
